5f84104fc06ae90c9c3665d2d67e4b1e91e9935c
zjxmolls <zjxmolls@outlook.com>
2026-07-23 20:28:16 +0800

    manual memory management

--- src/waster_lite_t2.cpp
+++ src/waster_lite_t2.cpp
@@ -1,8 +1,11 @@
+#include <future>
+
 //waster-lite for menory-save waster analysis
 
 #define DEBUG
 
 #include <cstdio>
+#include <cstdlib>
 #include <iostream>
 #include <string>
 #include <stdexcept>
@@ -13,6 +16,7 @@
 #include <array>
 #include <cmath>
 #include <bitset>
+#include <new>
 
 #include "sequence_utilities.hpp"
 
@@ -21,25 +25,28 @@ using std::tuple;
 using std::vector;
 using std::array;
 
-const int kSize = 21;
-const int kFlankSize = (int)(kSize-1) / 2;
-const unsigned long long B_SIZE = 1ULL<<29;
-const char EMPTY = 0b00;
+
+constexpr int kSize = 21;
+constexpr int kFlankSize = (int)(kSize-1) / 2;
+constexpr unsigned long long B_SIZE = 1ULL<<29;
+constexpr unsigned long long BT_SIZE = B_SIZE / 8;
+constexpr char EMPTY = 0b00;
+constexpr int ALIGNMENT = 1024;
+
+using BITSET = std::bitset<B_SIZE>;
+using EsTablePtr = BITSET* (*)[3];
+
+// char mem[12ULL<<30];
+char* mem = nullptr;
+char* membitarray[16][3];
+constexpr size_t MEM_SIZE = 12ULL << 30;
 
 typedef long long ll;
 
 static_assert(kFlankSize * 2 <= 64, "Sequence too long for 64-bit hash");
 
 struct HashSquare;
-struct Filter;
 struct BinSeq;
-
-ll SeqHash(const string&);
-string Hash2Seq(const ll);
-string RevComp(string);
-template<typename T> 
-T RevCompHash(T);
-inline HashSquare ProcessSingleMer(string);
 
 struct BinSeq{
     const static int K = 10, SHIFT = 2 * (K-1);
@@ -97,6 +104,19 @@ struct HashSquare {
     bool reverse, discard;
     ll AdjustedFlankHash, AdjustedCoreHash; 
 
+    template <typename T>
+    T RevCompHash(T x) {
+        constexpr int effective_bits = 2 * kFlankSize;  // 20
+        constexpr int pairs = effective_bits / 2;        // 10
+        T result = 0;
+        for (int i = 0; i < pairs; ++i) {
+            T pair = (x >> (effective_bits - 2 - 2*i)) & 0x03;
+            T rev = 3 - pair;
+            result |= (rev << (effective_bits - 2 - 2*i));
+        }
+        return result;
+    }
+
     HashSquare(ll n_, ll m_, ll c_) : n(n_), m(m_), c(c_) {
         discard = (n == m);
         if (!discard){
@@ -115,102 +135,6 @@ struct HashSquare {
     }
 };
 
-inline HashSquare ProcessSingleMer(string Mer){
-    if (Mer.size() != kSize){
-        throw std::invalid_argument("Invalid k-mer to process!");
-    }
-    auto nMer = Mer.substr(0, kFlankSize);
-    char cNuc = Mer[kFlankSize];
-    auto mMer = Mer.substr(kFlankSize+1, kFlankSize);
-
-    int c;
-    ll n, m;
-
-    switch(std::toupper(cNuc)){
-        case 'A': c = 0; break;
-        case 'C': c = 1; break;
-        case 'G': c = 2; break;
-        case 'T': c = 3; break;
-    }
-
-    n = SeqHash(nMer);
-    m = SeqHash(RevComp(mMer));
-
-    HashSquare hs(n, m, c);
-    return hs;
-}
-
-
-template <typename T>
-T RevCompHash(T x) {
-    constexpr int effective_bits = 2 * kFlankSize;  // 20
-    constexpr int pairs = effective_bits / 2;        // 10
-    T result = 0;
-    for (int i = 0; i < pairs; ++i) {
-        T pair = (x >> (effective_bits - 2 - 2*i)) & 0x03;
-        T rev = 3 - pair;
-        result |= (rev << (effective_bits - 2 - 2*i));
-    }
-    return result;
-}
-
-ll SeqHash(const string& seq) {
-    if (seq.size() != kFlankSize){
-        throw std::invalid_argument("Invalid sequence to build hash!");
-    }
-
-    ll hash = 0;
-    for (char nuc : seq) {
-        ll val;
-        switch (std::toupper(nuc)) {
-            case 'A': val = 0; break;
-            case 'C': val = 1; break;
-            case 'G': val = 2; break;
-            case 'T': val = 3; break;
-            default:
-                throw std::invalid_argument(std::format("Invalid nucleotide character: {}!", int(nuc)).c_str());
-        }
-        hash = (hash << 2) | val;
-    }
-    return hash;
-}
-
-string Hash2RCSeq(const ll n) {
-    string seq;
-    seq.reserve(kFlankSize);
-
-    ll tmp = n;
-    for (int i = 0; i < kFlankSize; ++i) {
-        char nuc;
-        switch (tmp & 3) {
-            case 0: nuc = 'T'; break; // A -> T
-            case 1: nuc = 'G'; break; // C -> G
-            case 2: nuc = 'C'; break; // G -> C
-            case 3: nuc = 'A'; break; // T -> A
-        }
-        seq.push_back(nuc);
-        tmp >>= 2;
-    }
-    return seq;
-}
-
-string RevComp(string Seq){
-    std::reverse(Seq.begin(), Seq.end());
-    string RCSeq = "";
-    for(auto fNuc : Seq){
-        char rNuc;
-        switch(std::toupper(fNuc)){
-            case 'A': rNuc = 'T'; break;
-            case 'T': rNuc = 'A'; break;
-            case 'C': rNuc = 'G'; break;
-            case 'G': rNuc = 'C'; break;
-            default: throw std::invalid_argument("Invalid nucleotide character!");
-        }
-        RCSeq += rNuc;
-    }
-    return RCSeq;
-}
-
 std::pair<ll, ll> recover_n_m(ll t, ll b, ll r) {
     ll k = (b * 65 + r) * 17 + t;
     ll lo = 0, hi = (1LL << 20);
@@ -224,9 +148,10 @@ std::pair<ll, ll> recover_n_m(ll t, ll b, ll r) {
     return {n, m};
 }
 
-template <typename Table>
-void FilterInputWorker(Table &FilterTable, string fileName, int fileorder){
-    std::cerr << "thread: " << fileorder << std::endl;
+// template <typename Table>
+void FilterInputWorker(char **FilterTable, string fileName, int fileorder){
+    // std::cerr << "thread: " << fileorder << std::endl;
+    std::cerr << std::format("[Task {}] - thread {} - initialized.\n", fileName, fileorder);
     SeqParser* seqfile = new SeqParser(fileName);
     while (seqfile->nextSeq()) {
         BinSeq* sequence = new BinSeq(seqfile->getSeq(1)); // fix to adapt for FASTQ real files.
@@ -236,15 +161,14 @@ void FilterInputWorker(Table &FilterTable, string fileName, int fileorder){
             HashSquare hs(n, m, EMPTY);
             
             if ((hs.discard || hs.t == 16) || (hs.t != fileorder)) continue;
-            FilterTable(hs.t, hs.b) = std::min(int(FilterTable(hs.t, hs.b)), int(hs.r));
+            FilterTable[hs.t][hs.b] = std::min((int)FilterTable[hs.t][hs.b], (int)hs.r);
         }
     }
-    std::cerr << std::format("[Task {}]", fileName) << "finished.\n";
+    std::cerr << std::format("[Task {}] finished.\n", fileName);
 
 }
 
-template <typename FTable, typename ETable>
-void CrossStatWorker(FTable &FilterTable, ETable &EsTable, string fileName, int fileorder){
+void CrossStatWorker(char **FilterTable, EsTablePtr EsTable, string fileName, int fileorder){
     std::cerr << std::format("This is task: {}\n", fileorder);
     SeqParser* seqfile = new SeqParser(fileName);
     while (seqfile->nextSeq()) {
@@ -253,25 +177,26 @@ void CrossStatWorker(FTable &FilterTable, ETable &EsTable, string fileName, int 
         while(sequence->Yieldable()){
             std::tie(n, m) = sequence->Yield();
             HashSquare hs(n, m, EMPTY);
-            
             if (hs.discard || hs.t == 16) continue;
-
-            if (hs.t >=0 && hs.t <4 && hs.t != fileorder && hs.r == FilterTable(hs.t, hs.b) && hs.r < 63){
-                (*EsTable[fileorder][hs.t])[hs.b] = 1;
+            if (hs.r == FilterTable[hs.t][hs.b] && hs.r < 63 && hs.t < 4){
+                if (EsTable[fileorder][hs.t]){
+                    (*EsTable[fileorder][hs.t])[hs.b] = 1;
+                }
             }
         }
     }
-    std::cerr << std::format("[task {}]: finished\n", fileorder);
+    std::cerr << std::format("[task {}] finished.\n", fileorder);
 }
 
-template <typename FTable, typename ETable>
-void StatDepoWorker(FTable &FilterTable, ETable &EsTable, int fileorder, ll (&bottle)[4]){
+void StatDepoWorker(char **FilterTable, EsTablePtr EsTable, int fileorder, ll (&bottle)[4]){
     std::cerr << std::format("This is task: {}\n", fileorder);
     for(ll b=0; b<B_SIZE; b++){
-        //128 = 2*2*2*2*2*2*2 = 10000000 so 127 = 1111111 64=1000000 63=111111
-        int res = ((int)(*EsTable[0][fileorder])[b] + (int)(*EsTable[1][fileorder])[b] + (int)(*EsTable[2][fileorder])[b] + (int)(*EsTable[3][fileorder])[b]);
+        int res = 0;
+        for (int i=0; i<4; i++){
+            res += EsTable[i][fileorder] ? (int)(*EsTable[i][fileorder])[b] : 0;
+        }
         bottle[res] += 1;
-        FilterTable(fileorder, b) = (res << 6) | FilterTable(fileorder, b);
+        FilterTable[fileorder][b] = (res << 6) | FilterTable[fileorder][b];
         if (b % 1000000 == 0){
             std::cerr << std::format("\r[task {}]: main loop time {}", fileorder, b);
         }
@@ -279,26 +204,41 @@ void StatDepoWorker(FTable &FilterTable, ETable &EsTable, int fileorder, ll (&bo
     std::cerr << std::format("\n[task {}]: finished\n", fileorder);
 }
 
+// struct WorkFlowc {
+//     WorkFlow {
+
+//     }
+
+
+// }
+
 int main(int argc, char** argv){
-    std::cerr<<"Initializing\n";
+    mem = static_cast<char*>(::operator new(MEM_SIZE, std::align_val_t(ALIGNMENT)));
+    // TO DELETE: ::operator delete(mem, std::align_val_t(ALIGNMENT));
+    if(!mem){
+        throw "Failed to allocate memory!";
+        exit(-1);
+    }
 
-    char* FTdata = new char[16 * (B_SIZE)];
+    
+    char *memstart = mem, *memEst = &mem[4LL << 30], *memFlt = memstart;
 
-    std::fill_n (FTdata, 16 * (B_SIZE), 63);
+    char *FilterTable[17];
+    for (int i = 0; i < 16; i++){
+        FilterTable[i] = mem + i * B_SIZE;
+    }
+    FilterTable[16] = nullptr;
+    std::fill_n (FilterTable[0], 16 * (B_SIZE), 63);
 
-    auto FilterTable = [&](size_t i, size_t j) -> char& {
-        return FTdata[i * (B_SIZE) + j];
-    };
 
     if(true){
         std::cerr << "Testing reads\n";
 
         std::vector<std::thread> threads;
         for (int i=1; i<17; i++){
-            std::cerr << "Initializing thread " << i << std::endl;
             std::string fileName = std::format("simc{}.fa", i);
-            threads.emplace_back(FilterInputWorker<decltype(FilterTable)>, 
-                std::ref(FilterTable), std::move(fileName), i-1);
+            threads.emplace_back(FilterInputWorker, 
+                FilterTable, std::move(fileName), i-1);
         }
 
         for (auto& t : threads) {
@@ -306,37 +246,53 @@ int main(int argc, char** argv){
         }
     }
 
+    char *EsStart = mem + 16 * B_SIZE;
+    BITSET *EsTable[16][3] = {};
+    for (int i = 0; i < 16; i++){
+        for (int j = 0; j < 3; j++){
+            EsTable[i][j] = new (EsStart + (i * 3 + j) * BT_SIZE) BITSET();
+        }
+    }
+
     if(true){
         std::cerr << "Estimating...\n";
-        std::vector<std::vector<std::unique_ptr<std::bitset<B_SIZE>>>> EsTable;
-        //4*16
-        std::cerr << "Initialized ES Table\n";
-        EsTable.reserve(4);
-        for (int i = 0; i < 4; ++i) {
-            EsTable.emplace_back(4);
-        }
 
-        for (auto& innerVec : EsTable) {
-            for (auto& ptr : innerVec) {
-                ptr = std::make_unique<std::bitset<B_SIZE>>();
-            }
+        using BITSET = std::bitset<B_SIZE>;
+        // BITSET *EsTable[4][4] = {};
+
+        BITSET *blockEsTable[4][17] = {};
+        int blockEsTableCnt[17] = {};
+        
+        int istart = 0; // 4, 8, 12
+        for (int istart = 0; istart < 16; istart += 4) {
+            for (int i = istart; i < istart + 4; ++i) { // i -> file
+                for (int j = istart; j < istart + 4; ++j) { // j -> t
+                    if (i != j){
+                        blockEsTable[i - istart][j] = EsTable[j][blockEsTableCnt[j]];
+                        blockEsTableCnt[j]++;
+                    }
+                }
+            }            
         }
         
         std::cerr << "Adding working threads: 4 CrossStatWorkers\n";
 
-        std::thread CSW1(CrossStatWorker<decltype(FilterTable), decltype(EsTable)>, std::ref(FilterTable), std::ref(EsTable), "simc1.fa", 0);
-        std::thread CSW2(CrossStatWorker<decltype(FilterTable), decltype(EsTable)>, std::ref(FilterTable), std::ref(EsTable), "simc2.fa", 1);
-        std::thread CSW3(CrossStatWorker<decltype(FilterTable), decltype(EsTable)>, std::ref(FilterTable), std::ref(EsTable), "simc3.fa", 2);
-        std::thread CSW4(CrossStatWorker<decltype(FilterTable), decltype(EsTable)>, std::ref(FilterTable), std::ref(EsTable), "simc4.fa", 3);
+        std::thread CSW1(CrossStatWorker, FilterTable, EsTable, string("simc1.fa"), 0);
+        std::thread CSW2(CrossStatWorker, FilterTable, EsTable, string("simc2.fa"), 1);
+        std::thread CSW3(CrossStatWorker, FilterTable, EsTable, string("simc3.fa"), 2);
+        std::thread CSW4(CrossStatWorker, FilterTable, EsTable, string("simc4.fa"), 3);
+
         CSW1.join(); CSW2.join(); CSW3.join(); CSW4.join();
 
         std::cerr << "Adding working threads: 4 StatDepoWorkers\n";
 
         ll res1[4] = {0,0,0,0}, res2[4] = {0,0,0,0}, res3[4] = {0,0,0,0}, res4[4] = {0,0,0,0};
-        std::thread SDW1(StatDepoWorker<decltype(FilterTable), decltype(EsTable)>, std::ref(FilterTable), std::ref(EsTable), 0, std::ref(res1));
-        std::thread SDW2(StatDepoWorker<decltype(FilterTable), decltype(EsTable)>, std::ref(FilterTable), std::ref(EsTable), 1, std::ref(res2));
-        std::thread SDW3(StatDepoWorker<decltype(FilterTable), decltype(EsTable)>, std::ref(FilterTable), std::ref(EsTable), 2, std::ref(res3));
-        std::thread SDW4(StatDepoWorker<decltype(FilterTable), decltype(EsTable)>, std::ref(FilterTable), std::ref(EsTable), 3, std::ref(res4));
+        
+        std::thread SDW1(StatDepoWorker, FilterTable, EsTable, 0, std::ref(res1));
+        std::thread SDW2(StatDepoWorker, FilterTable, EsTable, 1, std::ref(res2));
+        std::thread SDW3(StatDepoWorker, FilterTable, EsTable, 2, std::ref(res3));
+        std::thread SDW4(StatDepoWorker, FilterTable, EsTable, 3, std::ref(res4));
+
         SDW1.join(); SDW2.join(); SDW3.join(); SDW4.join();
 
         std::cerr << res1[0] << ' ' << res1[1] << ' ' << res1[2] << ' ' << res1[3] << std::endl;
